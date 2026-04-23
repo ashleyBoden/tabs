8b6a7f71ad3dc0f95a968b2cd7aa4dde84c12c88
unknown <ashboden@icloud.com>
2026-04-23 21:25:07 +0100

    Finished styling and added a second function to also apply a bottom border to the currently selected tab and change the font color of the other not in focus tabs.

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function displayTab(clickedButton) {
     var changeTab = document.querySelector("." + currentTab);
     changeTab.style.display = "block";
 };
+
+function highlightTab(clickedButton) {
+    document.querySelectorAll("button[id^='tab-']").forEach((btn) => {
+        btn.style.border = "0";
+        btn.style.color = "rgba(128, 128, 128, 0.822)";
+    })
+
+    var currentTab = clickedButton.id;
+    var selectedTab = document.querySelector("#" + currentTab);
+    selectedTab.style.borderBottom = "2px solid blue";
+    selectedTab.style.color = "black";
+}
